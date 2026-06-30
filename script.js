afba808bdbc0d38ea145ad1adcc78da2e63ac443
sudip-29 <sudipp2005@gmail.com>
2026-07-01 01:33:23 +0530

    Refactor HTML structure and improve styling for CA Pass Mark Calculator

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ function getMark(value) {
         return 0;
     }
 
-    let mark = parseFloat(value);
+    let mark = Number(value);
 
-    if (isNaN(mark)) {
+    if (isNaN(mark) || mark < 0 || mark > 25) {
         return 0;
     }
 
@@ -22,42 +22,44 @@ function calculate() {
     let ca3 = getMark(document.getElementById("ca3").value);
     let ca4 = getMark(document.getElementById("ca4").value);
 
-    let attend = parseInt(document.getElementById("attendance").value);
+    let attended = Number(document.getElementById("attendance").value);
 
     // Attendance Marks
-    let attendanceMarks = attend + 1;
+    let attendanceMarks = attended + 1;
 
-    // Best marks
-    let best12 = Math.max(ca1, ca2);
-    let best34 = Math.max(ca3, ca4);
+    // Best CA1 & CA2
+    let best1 = Math.max(ca1, ca2);
 
-    // Average
-    let average = (best12 + best34) / 2;
+    // Best CA3 & CA4
+    let best2 = Math.max(ca3, ca4);
 
-    // Internal Marks
+    // Average (whole number only)
+    let average = Math.floor((best1 + best2) / 2);
+
+    // Internal marks
     let internal = average + attendanceMarks;
 
-    // Marks required to pass
-    let required = 40 - internal;
+    // Marks needed to pass
+    let need = 40 - internal;
 
-    if (required < 0) {
-        required = 0;
+    if (need < 0) {
+        need = 0;
     }
 
-    document.getElementById("output").innerHTML = `
+    document.getElementById("result").innerHTML = `
         <h3>Result</h3>
         <hr><br>
 
-        <b>Best of CA1 & CA2:</b> ${best12}<br>
+        <b>Best of CA1 & CA2:</b> ${best1}<br>
 
-        <b>Best of CA3 & CA4:</b> ${best34}<br>
+        <b>Best of CA3 & CA4:</b> ${best2}<br>
 
-        <b>Average:</b> ${average.toFixed(2)}<br>
+        <b>Average (Whole Number):</b> ${average}<br>
 
         <b>Attendance Marks:</b> ${attendanceMarks}<br>
 
-        <b>Internal Marks:</b> ${internal.toFixed(2)} / 30<br><br>
+        <b>Internal Marks:</b> ${internal} / 30<br><br>
 
-        <h2>You need ${required.toFixed(2)} marks in the End Semester Exam to pass.</h2>
+        <h2 style="color:#0d6efd;">You need ${need} marks in the End Semester Exam to pass.</h2>
     `;
 }
